9cad4c6f921ae2ef665675f1aff6019fba819fcd
ResX-Official <chigozieebube9@gmail.com>
2025-06-14 08:29:55 -0700

    fix: install missing packages and correct controller imports

--- src/routes/propertyRoutes.ts
+++ src/routes/propertyRoutes.ts
@@ -1,11 +1,5 @@
 import { Router } from 'express';
-import {
-  createProperty,
-  getProperties,
-  getProperty,
-  updateProperty,
-  deleteProperty
-} from '../controllers/propertyController';
+import { createProperty, getProperties, getProperty, updateProperty, deleteProperty } from '../controllers/propertyController';
 import upload from '../utils/multer';
 
 const router = Router();
@@ -15,10 +9,5 @@ router.get('/', getProperties);
 router.get('/:id', getProperty);
 router.put('/:id', updateProperty);
 router.delete('/:id', deleteProperty);
-router.post('/upload', upload.array('images', 5), (req, res) => {
-  // req.files will be an array of uploaded files
-  const imageUrls = (req.files as any[]).map(file => file.path);
-  res.json({ imageUrls });
-});
 
 export default router;
